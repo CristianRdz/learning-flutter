be9c9084395ea16ff8e7e4c7fe3a0b41bb772f2a
CristianRdz <20213tn086@utez.edu.mx>
2024-09-30 14:18:12 -0600

    learning

---
+++ lib/widgets/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.cyan[50],
+      body: Center(
+        child: Image.asset('icon.png', width: 200, height: 200),
+      ),
+    );
+  }
+}
